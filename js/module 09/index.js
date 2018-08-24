40e5d12920a2db6963fd59dbe697571cd9d71248
Alexander Yatsenko <oldr.yatsenko@gmail.com>
2018-08-24 13:45:24 +0300

    fixed

--- js/module 09/index.js
+++ js/module 09/index.js
@@ -3,62 +3,74 @@
 const watches = document.querySelector('.wrapper');
 
 class Stopwatch {
-  constructor({
-    watches
-  }) {
+  constructor({ watches }) {
     this.watches = watches;
-    this.createStopwatch();
     this.isActive = false;
     this.startTime = null;
     this.deltaTime = null;
     this.id = null;
+    this.refs = {
+      container: document.querySelector('.watches'),
+      stopwatch: document.querySelector('.stopwatch'),
+      timerContent: document.querySelector('.js-time'),
+      startBtn: document.querySelector('.js-start'),
+      lapBtn: document.querySelector('.js-take-lap'),
+      resetBtn: document.querySelector('.js-reset'),
+      listLaps: document.querySelector('.js-laps'),
+    };
+    this.createStopwatch();
   }
 
   createStopwatch() {
-    const section = document.createElement('section')
-    section.classList.add('watches')
+    this.refs.container = document.createElement('section');
+    this.refs.container.classList.add('watches');
 
-    const div = document.createElement('div')
-    div.classList.add('stopwatch')
+    this.refs.stopwatch = document.createElement('div');
+    this.refs.stopwatch.classList.add('stopwatch');
 
-    const p = document.createElement('p');
-    p.classList.add('clockface')
-    p.classList.add('js-time')
-    p.textContent = '00:00.0'
+    this.refs.timerContent = document.createElement('p');
+    this.refs.timerContent.classList.add('stopwatch');
+    this.refs.timerContent.classList.add('clockface');
+    this.refs.timerContent.classList.add('js-time');
+    this.refs.timerContent.textContent = '00:00.0';
 
-    const startBtn = document.createElement('button');
-    startBtn.classList.add('timer-btn')
-    startBtn.classList.add('js-start')
-    startBtn.textContent = 'Start'
-    startBtn.addEventListener('click', this.handleStartTimer.bind(this));
+    this.refs.startBtn = document.createElement('button');
+    this.refs.startBtn.classList.add('timer-btn');
+    this.refs.startBtn.classList.add('js-start');
+    this.refs.startBtn.textContent = 'Start';
+    this.refs.startBtn.addEventListener(
+      'click',
+      this.handleStartTimer.bind(this),
+    );
 
-    const resetBtn = document.createElement('button');
-    resetBtn.classList.add('timer-btn')
-    resetBtn.classList.add('js-reset')
-    resetBtn.textContent = 'Reset'
-    resetBtn.addEventListener('click', this.hadleResetTimer.bind(this));
+    this.refs.resetBtn = document.createElement('button');
+    this.refs.resetBtn.classList.add('timer-btn');
+    this.refs.resetBtn.classList.add('js-reset');
+    this.refs.resetBtn.textContent = 'Reset';
+    this.refs.resetBtn.addEventListener(
+      'click',
+      this.hadleResetTimer.bind(this),
+    );
 
-    const lapBtn = document.createElement('button');
-    lapBtn.classList.add('timer-btn')
-    lapBtn.classList.add('js-take-lap')
-    lapBtn.textContent = 'Lap'
-    lapBtn.addEventListener('click', this.hadleLapTimer.bind(this));
+    this.refs.lapBtn = document.createElement('button');
+    this.refs.lapBtn.classList.add('timer-btn');
+    this.refs.lapBtn.classList.add('js-take-lap');
+    this.refs.lapBtn.textContent = 'Lap';
+    this.refs.lapBtn.addEventListener('click', this.hadleLapTimer.bind(this));
 
-    div.append(p, startBtn, lapBtn, resetBtn)
+    this.refs.stopwatch.append(
+      this.refs.timerContent,
+      this.refs.startBtn,
+      this.refs.lapBtn,
+      this.refs.resetBtn,
+    );
 
-    const list = document.createElement('ul');
-    list.classList.add('laps')
-    list.classList.add('js-laps')
+    this.refs.listLaps = document.createElement('ul');
+    this.refs.listLaps.classList.add('laps');
+    this.refs.listLaps.classList.add('js-laps');
 
-    section.append(div, list)
-    this.watches.append(section)
-
-    this.watch = this.watches.querySelector('.stopwatch');
-    this.startBtn = this.watches.querySelector('.js-start');
-    this.lapBtn = this.watches.querySelector('.js-take-lap');
-    this.resetBtn = this.watches.querySelector('.js-reset');
-    this.listLaps = this.watches.querySelector('.js-laps');
-    this.timerContent = this.watches.querySelector('.js-time');
+    this.refs.container.append(this.refs.stopwatch, this.refs.listLaps);
+    this.watches.append(this.refs.container);
   }
 
   startTimer() {
@@ -78,7 +90,7 @@ class Stopwatch {
   }
 
   updateClockface(time) {
-    this.timerContent.textContent = this.getFormattedTime(time);
+    this.refs.timerContent.textContent = this.getFormattedTime(time);
   }
 
   getFormattedTime(time) {
@@ -88,49 +100,47 @@ class Stopwatch {
     minutes = (minutes < 10 ? '0' : '') + minutes;
     let seconds = date.getSeconds();
     seconds = (seconds < 10 ? '0' : '') + seconds;
-    const mseconds = date.getMilliseconds().toString().slice(0, 1);
+    const mseconds = date
+      .getMilliseconds()
+      .toString()
+      .slice(0, 1);
     return `${minutes}:${seconds}.${mseconds}`;
   }
 
   setActiveBtn(target) {
     if (target.classList.contains('active')) return;
-    this.startBtn.classList.remove('active');
-    this.resetBtn.classList.remove('active');
+    this.refs.startBtn.classList.remove('active');
+    this.refs.resetBtn.classList.remove('active');
     target.classList.add('active');
-
   }
 
-  handleStartTimer({
-    target
-  }) {
+  handleStartTimer({ target }) {
     if (!this.isActive) {
       this.setActiveBtn(target);
       this.startTimer(target);
-      this.startBtn.textContent = 'Pause';
+      this.refs.startBtn.textContent = 'Pause';
     } else {
       this.pauseTimer(target);
-      this.startBtn.textContent = 'Continue';
+      this.refs.startBtn.textContent = 'Continue';
     }
   }
 
-  hadleResetTimer({
-    target
-  }) {
+  hadleResetTimer({ target }) {
     clearInterval(this.id);
     this.isActive = false;
     this.setActiveBtn(target);
     this.deltaTime = 0;
     this.updateClockface(this.deltaTime);
-    this.startBtn.textContent = 'Start';
-    this.listLaps.innerHTML = null;
+    this.refs.startBtn.textContent = 'Start';
+    this.refs.listLaps.innerHTML = null;
     this.startTime = null;
   }
 
   hadleLapTimer() {
     if (!this.isActive) return;
     const item = document.createElement('li');
-    item.textContent = this.timerContent.textContent;
-    this.listLaps.append(item);
+    item.textContent = this.refs.timerContent.textContent;
+    this.refs.listLaps.append(item);
   }
 }
 
