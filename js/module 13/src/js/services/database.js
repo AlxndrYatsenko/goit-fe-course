4a11a3f47979b04ebc83b94f09925a5a453b3522
Alexander Yatsenko <oldr.yatsenko@gmail.com>
2018-09-27 11:25:20 +0300

    edit cards

--- js/module 13/src/js/services/database.js
+++ js/module 13/src/js/services/database.js
@@ -1,10 +1,6 @@
 const BASE_URL = `http://api.linkpreview.net/?key=5bab6d746970f86da738753c41ba9d768c2bc1bc72d2b`
 
  const cards = [{
-      url: 'https://www.google.com.ua/',
-      img: 'http://www.google.com.ua/images/branding/googlelogo/1x/googlelogo_white_background_color_272x92dp.png',
-    },
-    {
       url: 'https://rozetka.com.ua/',
       img: 'https://i.rozetka.ua/favicons/apple-touch-icon-114x114.png',
     },
